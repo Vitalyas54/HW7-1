ff0b6470ef649a5351c10f85246a810262313c20
Vitaliy Bozhko <raptor1111@list.ru>
2022-02-12 17:56:51 +0700

    added method and test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -2,19 +2,25 @@ package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StatisticsServiceTest {
+    private StatisticsService service = new StatisticsService();
+    private long[] incomes = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+
+    @Test
+    void findMax() {
+        long expected = 12;
+        long actual = service.findMax(incomes);
+        assertEquals(expected, actual);
+    }
 
   @Test
-  void findMax() {
-    StatisticsService service = new StatisticsService();
-
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-    long expected = 12;
-
-    long actual = service.findMax(incomesInBillions);
-
+  void findMin() {
+    long expected = 3;
+    long actual = service.findMin(incomes);
     assertEquals(expected, actual);
+
+
   }
 }
