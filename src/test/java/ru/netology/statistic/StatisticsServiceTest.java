2b73c92a7227077ab4e4da159b49026775aa168a
Vitaliy Bozhko <raptor1111@list.ru>
2022-03-19 14:08:00 +0700

    added changes

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,17 +10,13 @@ class StatisticsServiceTest {
 
     @Test
     void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = { 5, 8, 4,12, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-        long actual = service.findMax(incomes);
+
+        long actual = service.findMax(incomesInBillions);
+
         assertEquals(expected, actual);
     }
-
-  @Test
-  void findMin() {
-    long expected = 3;
-    long actual = service.findMin(incomes);
-    assertEquals(expected, actual);
-
-
-  }
 }
